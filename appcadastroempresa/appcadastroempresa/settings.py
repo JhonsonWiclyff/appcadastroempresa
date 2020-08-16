23473f0004f24c561b6515eeebc2a56231963b13
ualassy <ualassy@gmail.com>
2020-08-16 16:56:26 -0300

    Teste pazuzu

--- appcadastroempresa/appcadastroempresa/settings.py
+++ appcadastroempresa/appcadastroempresa/settings.py
@@ -73,10 +73,21 @@ WSGI_APPLICATION = 'appcadastroempresa.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'appcadastroempresa',
+        'USER': 'admincompras',
+        'PASSWORD': 'admincompras',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
